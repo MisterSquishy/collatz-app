00a4a9a96162429d8c411a35e6f9557b908822c7
Peter Davids <peterdavids@Peters-MacBook-Pro.local>
2020-02-23 10:55:27 -0500

    this is so much better wih hooks

--- src/TheWholeThing/index.js
+++ src/TheWholeThing/index.js
@@ -1,95 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import IconButton from '@material-ui/core/IconButton'
 import ArrowRightIcon from '@material-ui/icons/ArrowRight'
 import Confetti from 'react-confetti'
 
-class TheWholeThing extends React.Component {
+const TheWholeThing = () => {
+  const [ subtext, setSubtext ] = useState("Enter a number")
+  const [ value, setValue ] = useState("")
+  const [ celebrate, setCelebrate ] = useState(false)
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      subtext: "Enter a number"
-    }
-  }
-
-  handleKeyDown = (event) => {
+  const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
-      this.handleClick()
+      handleClick()
     }
   }
 
-  handleChange = (event) => {
-    this.setState({
-      value: event.target.value,
-      celebrate: false,
-    })
+  const handleChange = (event) => {
+    setValue(event.target.value)
+    setCelebrate(false)
   }
 
-  handleClick = () => {
-    const { value } = this.state
+  const handleClick = () => {
     const intValue = parseInt(value)
+    // todo rewrite to compute collatz value in separate function and do it first
     if (!intValue || intValue < 0) {
-      this.setState({
-        value: "",
-        subtext: "Enter a number",
-      })
+      setValue("")
+      setSubtext("Enter a number")
     } else if (intValue === 1) {
       return
     } else if (intValue === 2) {
-      this.setState({
-        value: "1",
-        subtext: "Back to 1!",
-        celebrate: true,
-      })
+      setValue("1")
+      setSubtext("Back to 1!")
+      setCelebrate(true)
     } else if (intValue % 2) {
-      this.setState({
-        value: (3 * intValue) + 1,
-        subtext: `= 3 × ${intValue} + 1`,
-      })
+      setValue((3 * intValue) + 1)
+      setSubtext(`= 3 × ${intValue} + 1`)
     } else {
-      this.setState({
-        value: intValue / 2,
-        subtext: `= ${intValue} / 2`,
-      })
+      setValue(intValue / 2)
+      setSubtext(`= ${intValue} / 2`)
     }
   }
 
-  render() {
-    const {
-      value,
-      subtext,
-      celebrate
-    } = this.state
-
-    return <>
-      { celebrate &&
-        <Confetti
-        initialVelocityY="-5"
-        numberOfPieces="500"
-        wind="5"
-      /> }
-      <TextField
-        className="the-field"
-        type="Number"
-        placeholder="2"
-        helperText={ subtext }
-        value={ value }
-        onChange={ this.handleChange }
-        onKeyDown={ this.handleKeyDown }
-      />
-      <IconButton
-        aria-label="go"
-        size="large"
-        className="the-icon"
-        color="primary"
-        disabled={ celebrate }
-        onClick={ this.handleClick }
-      >
-        <ArrowRightIcon fontSize="inherit" />
-      </IconButton>
-    </>
-  }
+  return <>
+    { celebrate &&
+      <Confetti
+      initialVelocityY="-5"
+      numberOfPieces="500"
+      wind="5"
+    /> }
+    <TextField
+      className="the-field"
+      type="Number"
+      placeholder="2"
+      helperText={ subtext }
+      value={ value }
+      onChange={ handleChange }
+      onKeyDown={ handleKeyDown }
+    />
+    <IconButton
+      aria-label="go"
+      size="large"
+      className="the-icon"
+      color="primary"
+      disabled={ celebrate }
+      onClick={ handleClick }
+    >
+      <ArrowRightIcon fontSize="inherit" />
+    </IconButton>
+  </>
 }
 
 export default TheWholeThing
